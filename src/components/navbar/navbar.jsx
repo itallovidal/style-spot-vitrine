6bf734d124b21f186a5f0fdfb1e90d5300582fb3
itallovidal <itallovidalll@gmail.com>
2023-07-03 17:51:53 -0300

    navbar att

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,20 +3,32 @@ import './navbar.css'
 import {Link} from "react-router-dom";
 import categoriasInfo from "../../utilities/categoria.config.jsx";
 
+
+
+
+
+
+
 function Navbar(){
+    const nav = React.useRef()
+
+    function changeNav(){
+        nav.current.classList.toggle('changeNav')
+    }
+
     return (
         <header id={'container_navbar'}>
-            <div>
-                <p>Style  Spot</p>
+            <div id={'logo_container'}>
+                <p>Style <br/> &nbsp; Spot</p>
             </div>
 
-            <nav>
+            <i onClick={()=>{changeNav()}} id={'btn_menuOpen'} className="fa-solid fa-bars"></i>
+            <nav ref={nav}>
+                <i onClick={()=>{changeNav()}} id={'btn_menuClose'} className="fa-solid fa-xmark"></i>
                 <Link to={'/'}>Home</Link>
-                {
-                    categoriasInfo.map((info, i)=>{
+                {categoriasInfo.map((info, i)=>{
                         return <Link key={i} to={info.caminho}>{info.categoria}</Link>
-                    })
-                }
+                    })}
             </nav>
 
 
