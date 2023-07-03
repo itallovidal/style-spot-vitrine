a13fb2d2a99ef05b912f103b331e201626405585
itallovidal <itallovidalll@gmail.com>
2023-07-03 11:51:03 -0300

    filtro

--- src/pages/listagem/filtro.jsx
+++ src/pages/listagem/filtro.jsx
@@ -1,10 +1,93 @@
-import React from 'react';
-import { useParams} from "react-router-dom";
+import React, {useRef} from 'react';
+import {useParams} from "react-router-dom";
 
+export function Filtro({data}) {
 
-function Filtro({func}) {
+    const [filtroAtivo, setFiltroAtivo] = React.useState(null)
     const URL = useParams()
     let filtros = []
+    let produtosOriginais = useRef(data.produtos)
+
+    React.useEffect(()=>{
+        if(filtroAtivo){
+            handleFilter(filtroAtivo, produtosOriginais.current, data.setProdutos)
+        }
+        else{
+            data.setProdutos(produtosOriginais.current)
+            console.log('filtro desativado')
+        }
+    }, [filtroAtivo])
+
+    function handleFilter(filter, produtos, setProdutos){
+        let produtosFiltrados = []
+        const categoriasFiltro = {
+            'roupas': ['shirts', 'dresses', 'tops'],
+            'tenis': ['shoes'],
+            'relogios': ['watches'],
+            'joalheria': ['glasses', 'jewellery']
+        }
+
+
+        if(filter === 'masculino'){
+            for (const produto of produtos) {
+                for (const categoria of categoriasFiltro[URL.categoria]) {
+                    if(produto.category === `mens-${categoria}`)
+                        produtosFiltrados.push(produto)
+                }
+            }
+        }
+
+        if(filter === 'feminino'){
+            for (const produto of produtos) {
+                for (const categoria of categoriasFiltro[URL.categoria]) {
+                    if(produto.category === `womens-${categoria}`)
+                        produtosFiltrados.push(produto)
+                }
+            }
+        }
+
+        if(filter === 'caro'){
+            produtos.sort( (a, b)=>{
+                return b.price - a.price
+            })
+
+            produtosFiltrados = [...produtos]
+        }
+
+        if(filter === 'barato'){
+            produtosFiltrados = produtos.sort( (a, b)=>{
+                return a.price - b.price
+            })
+
+            produtosFiltrados = [...produtos]
+        }
+
+        if(filter === 'oculos'){
+            for (const produto of produtos) {
+
+                for (const categoria of categoriasFiltro[URL.categoria]) {
+                    if(produto.category === `sun${categoria}`)
+                    {
+                        produtosFiltrados.push(produto)
+                    }
+                }
+            }
+        }
+
+        if(filter === 'joia'){
+
+            for (const produto of produtos) {
+
+                for (const categoria of categoriasFiltro[URL.categoria]) {
+                    console.log(produto.category === categoria)
+                    if(produto.category === `womens-${categoria}`)
+                        produtosFiltrados.push(produto)
+                }
+            }
+        }
+
+        setProdutos(produtosFiltrados)
+    }
 
     switch (URL.categoria){
         case 'roupas':
@@ -18,17 +101,30 @@ function Filtro({func}) {
             break
     }
 
-
-
+// 1E1E1EFF
     return (
         <section id={'container_filtro'}>
             {
                 filtros.map((filtro, i)=>{
-                    return <span key={i} onClick={func} className={'btn_filtro'}>{filtro}</span>
+                    return <button key={i}
+                                   onClick={(e)=> {
+                                       setFiltroAtivo((prev)=>{
+                                           console.log(e.target.textContent.toLowerCase())
+                                           if( prev !== null && prev === e.target.textContent.toLowerCase()){
+                                               return null
+                                           }
+                                           else if(prev !== null && prev !== e.target.textContent.toLowerCase()){
+                                               return e.target.textContent.toLowerCase()
+                                           }
+                                           else{
+                                               return e.target.textContent.toLowerCase()
+                                           }
+                                       })
+                                   }}
+                                   className={'btn_filtro'}>{filtro}</button>
                 })
             }
         </section>
     );
 }
 
-export default Filtro;
--- src/pages/listagem/listagemProdutos.jsx
+++ src/pages/listagem/listagemProdutos.jsx
@@ -4,7 +4,7 @@ import getItems from "../../utilities/getItems.jsx";
 import './listagemProdutos.css'
 import CardProduto from "../../components/cardProduto/cardProduto.jsx";
 import Loading from "../../components/loading/loading.jsx";
-import Filtro from "./filtro.jsx";
+import {Filtro} from "./filtro.jsx";
 
 const query = {
     'roupas': ['womens-dresses', 'mens-shirts', 'tops'],
@@ -21,21 +21,13 @@ const categoriaImagens = {
 }
 
 async function getProducts(categorias){
-
     let produtos = []
-
     for (const categoria of categorias) {
         const resultado = await getItems( categoria, 5)
         produtos = [...produtos, ...resultado]
     }
-
     return produtos
 }
-
-
-
-
-
 
 
 function ListagemProdutos() {
@@ -51,60 +43,13 @@ function ListagemProdutos() {
     }, [url])
 
 
-    function handleFilter(e){
-        const filter = e.target.textContent.toLowerCase()
-        let produtosFiltrados = []
-        const categoriasFiltro = {
-            'roupas': ['shirts', 'dresses', 'tops'],
-            'tenis': ['shoes'],
-            'relogios': ['watches'],
-        }
-
-        if(filter === 'masculino'){
-            for (const produto of produtos) {
-                for (const categoria of categoriasFiltro[url.categoria]) {
-                    if(produto.category === `mens-${categoria}`)
-                        produtosFiltrados.push(produto)
-                }
-            }
-        }
-
-        if(filter === 'feminino'){
-            for (const produto of produtos) {
-                for (const categoria of categoriasFiltro[url.categoria]) {
-                    if(produto.category === `womens-${categoria}`)
-                        produtosFiltrados.push(produto)
-                }
-            }
-        }
-
-        if(filter === 'caro'){
-            produtos.sort( (a, b)=>{
-                return b.price - a.price
-            })
-
-            produtosFiltrados = [...produtos]
-        }
-
-        if(filter === 'barato'){
-            produtosFiltrados = produtos.sort( (a, b)=>{
-                return a.price - b.price
-            })
-
-            produtosFiltrados = [...produtos]
-        }
-
-        setProdutos(produtosFiltrados)
-    }
-
-
     return produtos !== null ? (
-        <main key={produtos[0].category} id={'container_listagemProdutos'}>
+        <main id={'container_listagemProdutos'}>
             <header id={'header_listagem'}>
                 <picture>
                     <img className={produtos[0].category} src={categoriaImagens[url.categoria]} alt=""/>
                 </picture>
-                <Filtro func={handleFilter}/>
+                <Filtro data={{produtos, setProdutos}} />
             </header>
 
             <article id={'listagem'}>
